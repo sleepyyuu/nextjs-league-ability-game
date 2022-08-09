a36d2af68ec72378f5976a389ef70ef8eb5ac768
sleepyyuu <k28143995@gmail.com>
2022-08-08 20:24:03 -0700

    Populate answerbox on selection

--- components/Game/GuessBox.js
+++ components/Game/GuessBox.js
@@ -5,30 +5,29 @@ import Image from "next/image";
 
 export default function GuessBox(props) {
   let { currentGuessRow, setCurrentGuessRow } = props;
-  const [currentGuess, setCurrentGuess] = useState([{}, {}, {}, {}, {}]);
+  let fillerGuesses = new Array(5 - currentGuessRow.length).fill({});
   return (
     <div className={styles.guessBoxContainer}>
       {currentGuessRow.map((guessAbility) => {
-        if (currentGuess.length > 0) {
-          return (
-            <div key={uniqid()} className={styles.guessBox}>
-              <Image
-                src={"/images/spell/" + guessAbility.image.full}
-                width={100}
-                height={100}
-                onClick={() => {
-                  //handle remove
-                }}
-              ></Image>
-            </div>
-          );
-        } else {
-          return (
-            <div key={uniqid()} className={styles.guessBox}>
-              adg
-            </div>
-          );
-        }
+        return (
+          <div key={uniqid()} className={styles.guessBox}>
+            <Image
+              src={"/images/spell/" + guessAbility.image.full}
+              width={100}
+              height={100}
+              onClick={() => {
+                //handle remove
+              }}
+            ></Image>
+          </div>
+        );
+      })}
+      {fillerGuesses.map(() => {
+        return (
+          <div className={styles.guessBox} key={uniqid()}>
+            {}
+          </div>
+        );
       })}
     </div>
   );
