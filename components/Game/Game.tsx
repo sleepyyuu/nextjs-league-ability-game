5ab4d64931f5684ae8e588d0901600d3406bd80c
sleepyyuu <k28143995@gmail.com>
2022-08-08 19:44:25 -0700

    Randomized answer options

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -1,39 +1,43 @@
 import GuessBox from "./GuessBox";
 import GuessOptions from "./GuessOptions";
 import championListData from "../../assets/data/champion.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Game() {
-  const championArray = Object.keys(championListData.data);
-  const answerChampionNumber = Math.floor(Math.random() * championArray.length);
-  let selectedChamp = require("../../assets/data/champion/" + championArray[answerChampionNumber] + ".json").data;
-  selectedChamp = selectedChamp[Object.keys(selectedChamp)[0]];
-
-  let additionalAbilityChoices = [];
-  while (additionalAbilityChoices.length < 8) {
-    let randomChampionNumber = Math.floor(Math.random() * championArray.length);
-    if (randomChampionNumber === answerChampionNumber) {
-      continue;
-    } else {
-      let additionalSelectedChamp = require("../../assets/data/champion/" + championArray[randomChampionNumber] + ".json").data;
-      additionalSelectedChamp = additionalSelectedChamp[Object.keys(additionalSelectedChamp)[0]];
-      let randomAbilitySelect = Math.floor(Math.random() * 4);
-      if (additionalAbilityChoices.find((ability) => ability.name === additionalSelectedChamp.spells[randomAbilitySelect].name)) {
+  const [selectedChampion, setSelectedChampion] = useState({ name: "" });
+  const [selectedChampionAbilities, setSelectedChampionAbilities] = useState([]);
+  const [abilityOptions, setAbilityOptions] = useState([]);
+  useEffect(() => {
+    const championArray = Object.keys(championListData.data);
+    const answerChampionNumber = Math.floor(Math.random() * championArray.length);
+    let selectedChamp = require("../../assets/data/champion/" + championArray[answerChampionNumber] + ".json").data;
+    selectedChamp = selectedChamp[Object.keys(selectedChamp)[0]];
+    setSelectedChampion(selectedChamp);
+    setSelectedChampionAbilities(selectedChamp.spells);
+    let additionalAbilityChoices = [];
+    while (additionalAbilityChoices.length < 8) {
+      let randomChampionNumber = Math.floor(Math.random() * championArray.length);
+      if (randomChampionNumber === answerChampionNumber) {
         continue;
       } else {
-        additionalAbilityChoices.push(additionalSelectedChamp.spells[randomAbilitySelect]);
+        let additionalSelectedChamp = require("../../assets/data/champion/" + championArray[randomChampionNumber] + ".json").data;
+        additionalSelectedChamp = additionalSelectedChamp[Object.keys(additionalSelectedChamp)[0]];
+        let randomAbilitySelect = Math.floor(Math.random() * 4);
+        if (additionalAbilityChoices.find((ability) => ability.name === additionalSelectedChamp.spells[randomAbilitySelect].name)) {
+          continue;
+        } else {
+          additionalAbilityChoices.push(additionalSelectedChamp.spells[randomAbilitySelect]);
+        }
       }
     }
-  }
-
-  const [selectedChampion, setSelectedChampion] = useState(selectedChamp);
-  const [selectedChampionAbilities, setSelectedChampionAbilities] = useState(selectedChamp.spells);
-  const [abilityOptions, setAbilityOptions] = useState([...selectedChamp.spells, ...additionalAbilityChoices]);
-  console.log(abilityOptions);
+    setAbilityOptions([...selectedChamp.spells, ...additionalAbilityChoices]);
+  }, []);
+  console.log(selectedChampion.name);
   return (
     <div>
+      <div>Selected champion is {selectedChampion ? selectedChampion.name : ""}</div>
       <GuessBox></GuessBox>
-      <GuessOptions></GuessOptions>
+      <GuessOptions abilityOptions={abilityOptions}></GuessOptions>
     </div>
   );
 }
--- components/Game/GuessOptions.tsx
+++ components/Game/GuessOptions.tsx
@@ -1,10 +1,38 @@
-import * as championListData from "../../assets/data/champion.json";
-import { useState } from "react";
+import Image from "next/image";
+import uniqid from "uniqid";
+import styles from "../../styles/GuessOptions.module.scss";
 
-export default function GuessOptions() {
+export default function GuessOptions(props) {
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+  let { abilityOptions } = props;
+  shuffleArray(abilityOptions);
+  let abilityRowArray = [abilityOptions.slice(0, 4), abilityOptions.slice(4, 8), abilityOptions.slice(8, 12)];
+
   //pick random champion
   //get all abilities/icons for that champion
   //get 5 more random icons from any, no dupes
   //array of icons, if any icons random number match, get another num
-  return <div></div>;
+  return (
+    <div className={styles.guessOptionContainer}>
+      {abilityRowArray.map((abilityRow, abilityRowIndex) => {
+        return (
+          <div key={abilityRowIndex} className={styles.abilityRow}>
+            {abilityRow.map((ability, abilityIndex) => {
+              let imageSource = "/images/spell/" + ability.image.full;
+              return (
+                <div key={abilityIndex} suppressHydrationWarning className={styles.abilityImageContainer}>
+                  <Image src={imageSource} width={100} height={100}></Image>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
